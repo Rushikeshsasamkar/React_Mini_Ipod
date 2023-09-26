2463dfb9fa67fcf7316de2c085aa50bdf1dfddd1
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2023-09-27 00:39:20 +0530

    first commit

---
+++ src/assets/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./TereHawale .mp3";
+import music2 from "./IshqMein.mp3";
+import music3 from "./harfanMaula.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
